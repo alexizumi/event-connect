3e0dd12e81858ae3ba3e2a88a1047c0d74d859f9
alexizumi <alexizumi12@gmail.com>
2025-06-09 10:57:13 +0100

    Default image bug fixed

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,4 +1,4 @@
 // File: src/pages/Auth/SignUp.tsx
 
-export const DEFAULT_EVENT_IMAGE = '../../public/No-Image.png';
-export const DEFAULT_EVENT_IMAGE_LARGE = '../../public/No-Image.png';
+export const DEFAULT_EVENT_IMAGE = '/No-Image.png';
+export const DEFAULT_EVENT_IMAGE_LARGE = '/No-Image.png';
